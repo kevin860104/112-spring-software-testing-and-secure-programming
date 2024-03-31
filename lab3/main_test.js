1c5e8c2221f4b8cfc8c316e3a7fc022e923bc0f5
kevin860104 <55239515+kevin860104@users.noreply.github.com>
2024-03-31 20:46:06 +0800

    Update main_test.js

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -4,19 +4,20 @@ const { Calculator } = require('./main');
 
 describe("Calculator Test", () => {
     const calculator = new Calculator();
+    
     const logTestSuites = [
         {
             operation: "log",
             cases: [
-	{ data: 9, expected: Math.log(9) },
-	{ data: 6, expected: Math.log(6) },
-	{ data: 1, expected: Math.log(1) },
-	{ data: 'kevin', expected: Error, message: "unsupported operand type" },
-	{ data: true, expected: Error, message: "unsupported operand type" },
-	{ data: Infinity, expected: Error, message: "unsupported operand type" },
-	{ data: 0, expected: Error, message: "math domain error (1)" },
-	{ data: -1, expected: Error, message: "math domain error (2)" },
-		]
+                { data: 9, expected: 2.1972245773362196 },
+                { data: 6, expected: 1.791759469228055 },
+                { data: 1, expected: 0 },
+                { data: 'kevin', expected: Error, message: "unsupported operand type" },
+                { data: true, expected: Error, message: "unsupported operand type" },
+                { data: Infinity, expected: Error, message: "unsupported operand type" },
+                { data: 0, expected: Error, message: "math domain error (5)" },
+                { data: -1, expected: Error, message: "math domain error (3)" },
+            ]
         }
     ];
 
@@ -32,13 +33,14 @@ describe("Calculator Test", () => {
             });
         });
     });
+
     const expTestSuites = [
         {
             operation: "exp",
             cases: [
-                { data: 8, expected: Math.exp(8) },
-                { data: 0, expected: Math.exp(0) },
-                { data: -3, expected: Math.exp(-3) },
+                { data: 8, expected: 2980.9579870417283 },
+                { data: 0, expected: 1 },
+                { data: -3, expected: 0.049787068367863944 },
                 { data: 'kkk', expected: Error, message: "unsupported operand type" },
                 { data: true, expected: Error, message: "unsupported operand type" },
                 { data: Infinity, expected: Error, message: "unsupported operand type" },
