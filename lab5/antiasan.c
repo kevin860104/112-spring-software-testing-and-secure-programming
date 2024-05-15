4256b72f65974b6a9610f08c0b708f0062aa0c90
kevin860104 <55239515+kevin860104@users.noreply.github.com>
2024-05-15 12:36:12 +0800

    Update antiasan.c

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,10 +1,16 @@
 #include <stdint.h>
 
-void antiasan(uintptr_t addr)
+void modifyMemory(uintptr_t address)
 {
-    if (addr == 0) {
+    if (address == 0) {
+        // 防止解引用空指針
         return;
     }
-    char* add = (char *)((addr >> 3) | 0x7fff8000);
-    *add = 0x00;
+    
+    // 計算實際的記憶體地址，並保證位址計算過程的正確性
+    uintptr_t calculatedAddress = (address >> 3) | 0x7FFF8000;
+    char* targetAddress = (char *)calculatedAddress;
+
+    // 設置目標地址的值為 0
+    *targetAddress = 0x00;
 }
